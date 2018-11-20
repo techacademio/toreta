2d860d4d9c9db218c356e77c37552fcccf64510e
tech.academio <techacademio@gmail.com>
2018-11-20 11:45:14 +0000

    company registration

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,27 @@
+class CompaniesController < ApplicationController
+  def show
+    @company = Company.find(@params[:id])
+  end
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(company_params)
+    
+    if @company.save
+      flash[:success] = '会員登録しました'
+      redirect_to @company
+    else
+      flash.now[:danger] = '会員登録に失敗しました'
+      render :new
+    end
+  end
+  
+  private
+  
+  def company_params
+    params.require(:company).permit(:name, :email, :password, :password_confirmation)
+  end
+end
